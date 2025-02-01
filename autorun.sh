f1f96471724ccfd9236a0f219c0630573047f759
benmoose39 <unnik1998@gmail.com>
2025-02-01 15:17:46 +0530

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -6,6 +6,7 @@ python3 -m pip install requests datetime
 
 cd $(dirname $0)/
 
+wget https://dl.dropboxusercontent.com/s/toq87dmqp5eh2b2/IPTV_tuner_epg.xml
 python3 capture.py
 
 echo $(date) > lastUpdated
